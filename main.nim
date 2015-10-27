aab0b39b4cfb0b4bde4b1eefc57116d3e52d9fe2
Andrew Penkrat <contact.aldrog@gmail.com>
2015-10-27 14:19:25 +0300

    Fix source function being passed wrong way.

--- main.nim
+++ main.nim
@@ -9,24 +9,24 @@ const
     aStart = -1.0
     aEnd = 1.0
     
-    source: (proc(x: float): float) = f
-
-proc eqnodes(): Vector64[nodeCount] =
-    new result
-    for i in 0..(nodeCount - 1):
-        result[i] = aStart + i.float * (aEnd - aStart) / (nodeCount - 1)
-
-proc values[N: static[int]](function: (proc(x: float): float), x: Vector64[N]): Vector64[N] =
+proc eqnodes(N: static[int]): Vector64[N] =
     new result
     for i in 0..(N - 1):
-        result[i] = function(x[i])
+        result[i] = aStart + i.float * (aEnd - aStart) / (N - 1)
+
+proc values[N: static[int]](function: (proc(x: float): float), points: Vector64[N]): Vector64[N] =
+    new result
+    for i in 0..(N - 1):
+        result[i] = function(points[i])
 
 let
-    x = eqnodes()
+    x = eqnodes(nodeCount)
     #x = vector([-1.0, -0.5, 0.0, 0.5, 1.0])
-    y = source.values(x)
+    y = f.values(x)
 
-let approx: (proc(x: float): float) = leastSquares(x, y)
+let 
+    approx: (proc(x: float): float) = leastSquares(x, y)
+    source: (proc(x: float): float) = f
 
 showGraph(@[source, approx])
 
