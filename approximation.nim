08d127c6fe08e23f5eee74b91f4cf2b4f9d53efd
Andrew Penkrat <contact.aldrog@gmail.com>
2015-10-27 17:35:01 +0300

    Implement solving equation systems with Gauss method.

--- approximation.nim
+++ approximation.nim
@@ -1,8 +1,27 @@
 import
     math, linalg
 
-proc solveWithGauss[M, N](A: Matrix64[M, M], b: Vector64[N]): Vector64[M] =
-    zeros(M)
+proc solveWithGauss[M, N](A: Matrix64[M, N], b: Vector64[M]): Vector64[N] =
+    var expandedMatrix = makeMatrix(M, N + 1,
+        proc(i, j: int): float = (if j < N: A[i, j] else: b[i]),
+        order = rowMajor)
+    echo expandedMatrix
+    var emRows: array[M, Vector64[N + 1]]
+    for i in 0..(M - 1):
+        emRows[i] = expandedMatrix.rowUnsafe(i)
+
+    for i in 0..(M - 2):
+        for j in (i + 1)..(M - 1):
+            emRows[j] -= emRows[i] * (emRows[j][i] / emRows[i][i])
+    echo expandedMatrix
+    for i in countdown(M - 1, 1):
+        for j in 0..(i - 1):
+            emRows[j] -= emRows[i] * (emRows[j][i] / emRows[i][i])
+    echo expandedMatrix
+    for i in 0..(M - 1):
+        emRows[i] /= emRows[i][i]
+    echo expandedMatrix
+    return expandedMatrix.column(N)
 
 proc leastSquares*[N](x: Vector64[N], y: Vector64[N]): (proc(x: float): float) =
     let 
